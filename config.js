b33b1110b1cc61ece5395325994461cd1ceb2fec
Dimitry Ivanov <2@ivanoff.org.ua>
2019-03-17 23:22:35 +0200

    up

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
-  models: {
+  models: [
     stars: { name: 'string', birth: 'date' },
     movies: { name: 'string' },
     comments: { title: 'string', comment: { type: 'string', required: true } },
-  },
+  ],
 
   links: {
     movies: ['stars', 'comments'],
